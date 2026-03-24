a9a3fe02a2f933a56f9b7a2bb6e419a76ad7d43f
BENJAMINDTS <benjamin.pk02@gmail.com>
2026-03-24 19:08:28 +0100

    fix(responsive): add px-4 base padding to container in ingredients/index

--- resources/views/ingredients/index.blade.php
+++ resources/views/ingredients/index.blade.php
@@ -6,7 +6,7 @@
   </x-slot>
 
   <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="p-6 text-gray-900 dark:text-gray-100">
         <div class="flex justify-end mb-4">
           <a href="{{ route('ingredients.create') }}" class="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
