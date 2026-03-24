9ad363f05b1610c4fdf3fbe15b2f0a71277d0850
BENJAMINDTS <benjamin.pk02@gmail.com>
2026-03-24 19:08:18 +0100

    fix(responsive): add px-4 base padding to container in categories/edit

--- resources/views/categories/edit.blade.php
+++ resources/views/categories/edit.blade.php
@@ -7,7 +7,7 @@
   </x-slot>
 
   <div class="py-12">
-    <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
         <form action="{{ route('categories.update', $category) }}" method="POST" class="space-y-6">
           @csrf
