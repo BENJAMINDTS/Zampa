a0460209283408ac83e407c0b5baf140b0b5af90
SebastianBCF <secafer06@gmail.com>
2026-04-20 22:44:40 +0200

    test(setup): configure Pest.php global bootstrap and CreatesTenants trait

---
+++ tests/Pest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
+
+uses(RefreshDatabase::class)->in('Feature');
+
+beforeEach(function () {
+    Http::preventStrayRequests();
+
+    Http::fake([
+        'api.groq.com/*' => Http::response([
+            'choices' => [
+                ['message' => ['content' => 'Respuesta mock del chatbot']],
+            ],
+            'usage' => ['total_tokens' => 50],
+        ], 200),
+    ]);
+});
