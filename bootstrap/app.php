093f1d5961190f07af6bb03a7955a202f59fe070
Ayrton <adalaniaf@gmail.com>
2026-04-22 11:21:42 +0200

    feat: enable throttleApi middleware on API routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
         $middleware->alias(['role' => \App\Http\Middleware\EnsureRole::class]);
+        $middleware->throttleApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
