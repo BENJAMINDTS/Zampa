4e2992697f2774a1c7e32c0a6cb4fb214b2044fa
Ayrton <adalaniaf@gmail.com>
2026-04-25 15:39:05 +0200

    feat(bloque6.4): create TapaConfigFactory

---
+++ database/factories/TapaConfigFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TapaConfig>
+ *
+ * @author BenjaminDTS
+ */
+class TapaConfigFactory extends Factory
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id'           => User::factory(),
+            'tapas_enabled'     => false,
+            'tapas_free'        => true,
+            'max_tapa_variants' => 3,
+            'tapa_price'        => null,
+        ];
+    }
+}
