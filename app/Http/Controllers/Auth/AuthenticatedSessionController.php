ac65e92e849af6c411c87c5c15cd1efb3d37b898
BENJAMINDTS <benjamin.pk02@gmail.com>
2026-05-03 15:02:58 +0200

    fix: redirect superadmin to superadmin.dashboard after login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,6 +28,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        if (Auth::user()->isSuperAdmin()) {
+            return redirect()->intended(route('superadmin.dashboard'));
+        }
+
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
