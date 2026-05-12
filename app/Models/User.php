98a584bc0fba94f5ace2b702e0c5d913c5eea1bb
SebastianBCF <secafer06@gmail.com>
2026-05-12 21:23:33 +0200

    fix(huerfanos): add SoftDeletes trait to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Order;
@@ -32,11 +33,13 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
  * @property float|null  $lng            Longitud del negocio
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
  * @author BenjaminDTS
+ * @author SebastianBCF
  */
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * Los atributos que se pueden asignar masivamente.
