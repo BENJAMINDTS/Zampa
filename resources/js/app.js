67e7e5be083aa80956b5731e9078cd1a0e2c1723
BENJAMINDTS <benjamin.pk02@gmail.com>
2026-05-26 20:06:56 +0200

    refactor: register all extracted Alpine components in app.js alpine:init listener

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,28 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import { registerLayoutBadges }  from './alpine/layout-badges.js';
+import { registerKitchenPanel }  from './alpine/kitchen-panel.js';
+import { registerBarComponents } from './alpine/bar-panel.js';
+import { registerBusinessConfig } from './alpine/business-config.js';
+import { registerCart }          from './alpine/cart.js';
+import { registerMenuFilters }   from './alpine/menu-filters.js';
+import { registerBill }          from './alpine/bill.js';
+import { registerChatWidget }    from './alpine/chat-widget.js';
+import { registerTableMap }      from './pages/table-map.js';
+
 window.Alpine = Alpine;
+
+document.addEventListener('alpine:init', () => {
+    registerLayoutBadges();
+    registerKitchenPanel();
+    registerBarComponents();
+    registerBusinessConfig();
+    registerCart();
+    registerMenuFilters();
+    registerBill();
+    registerChatWidget();
+    registerTableMap();
+});
 
 Alpine.start();
