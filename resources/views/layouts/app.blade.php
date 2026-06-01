26fabe47d72d322e17d62c4344df10a5039a6088
BENJAMINDTS <benjamin.pk02@gmail.com>
2026-06-01 12:35:37 +0200

    feat(landing): add Zampi favicon to authenticated app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Zampa') }}</title>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="shortcut icon" href="/favicon.svg">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
